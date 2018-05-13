3e68f0b8c0fe23ee888a06ed3b2bca1b6dc40aef
Arika <Delete0093@Gmail.com>
2018-05-13 22:56:48 +0900

    update for puppeteer-buildpack (https://github.com/CoffeeAndCode/puppeteer-heroku-buildpack)

--- server/app/webCapture.js
+++ server/app/webCapture.js
@@ -34,6 +34,7 @@ module.exports = {
 	launchBrowser: async function() {
 		this._browser = await puppeteer.launch({
 			headless: true,
+			args: ['--no-sandbox', '--disable-setuid-sandbox'],
 		});
 	},
 
